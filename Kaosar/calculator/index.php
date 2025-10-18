753369373ab5ffe200865bf7dde748419652df2c
Kaosar Ali <kaosar3662@gmail.com>
2025-10-18 18:52:42 +0600

    Made a small calculatorusing php

--- Kaosar/calculator/index.php
+++ Kaosar/calculator/index.php
@@ -11,7 +11,7 @@
 <body>
   <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
     <input type="number" name="num01" placeholder="number one">
-    <select name="operater">
+    <select name="operator">
       <option value="add">+</option>
       <option value="subtract">-</option>
       <option value="multiply">*</option>
@@ -22,9 +22,37 @@
   </form>
 
   <?php
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $num1 = htmlspecialchars($_POST["num01"]);
+    $num2 = htmlspecialchars($_POST["num02"]);
+    $sign = htmlspecialchars($_POST["operator"]);
 
+    $err = false;
+    if (empty($num1) || empty($num2)) {
+      echo "<p class='calc-error'>Fill in all fields!</p>";
+      $err = true;
     }
+    if (!is_numeric($num1) || !is_numeric($num2)) {
+      echo "<p class='calc-error'>You are allowed to type numbers only!</p>";
+      $err = true;
+    }
+
+    if ($err == false) {
+      $value = 0;
+      if ($sign == "add") {
+        $value = $num1 + $num2;
+      } elseif ($sign == "subtract") {
+        $value = $num1 - $num2;
+      } elseif ($sign == "multiply") {
+        $value = $num1 * $num2;
+      } elseif ($sign == "divide") {
+        $value = $num1 / $num2;
+      } else {
+        echo "Something is wrong here";
+      }
+      echo "<h1>Result is $value</h1>";
+    }
+  }
   ?>
 
 </body>
